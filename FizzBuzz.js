da611bf737b6d7e9307a4662f652c1723b2d1419
Yvy Bakker <yvybakker@gmail.com>
2019-04-26 16:40:28 +0200

    Finished

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,15 @@
-
 var i;
-for (i = 0; i <= 100; i++) { 
+for (i = 1; i <= 100; i++) { 
 
-if (i==3){
+
+if (i % 3 === 0) {
   console.log("Fizz");
 }
-else if (i==5){
-  console.log("Buzz");
+
+else if (i % 5 === 0){
+ console.log("Buzz");
 }
+
 else {
   console.log(i)
 }
